9b1f35f9156dfc7833146cdae14478ac1f17e945
kaeky <carlosbasan2011@hotmail.com>
2024-12-04 22:10:54 -0500

    feat: Add micro tasks and add verify token Endpoint

--- micro-user/src/auth/auth.controller.ts
+++ micro-user/src/auth/auth.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor() {}
+
+  @Post('verifyToken')
+  @UseGuards(JwtAuthGuard)
+  async verifyToken(@Request() req) {
+    return req.user;
+  }
 }
